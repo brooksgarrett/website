525f4107e9792a977dab529797768000207eaae2
Brooks Garrett <brooks.garrett@hp.com>
2016-02-28 15:24:08 -0500

    Try using Travis-CI again.

---
+++ bin/linux-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_386.tar.gz \
+  -O ./bin/hugo.tgz
+mv hugo.tgz ./bin/
+cd ./bin
+tar xzf hugo.tgz
+mv hugo_0.15_linux_386\hugo_0.15_linux_386 hugo
+chmod +x hugo
