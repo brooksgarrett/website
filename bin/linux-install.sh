069b49ac21900a9c90bffa7ab0f00b2ce65e137f
Brooks Garrett <brooks@brooksgarrett.com>
2017-03-10 12:48:05 -0500

    Upgrade to hugo 0.19

--- bin/linux-install.sh
+++ bin/linux-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_386.tar.gz \
+wget https://github.com/spf13/hugo/releases/download/v0.19/hugo_0.19_Linux-32bit.tar.gz \
   -O ./bin/hugo.tgz
 cd ./bin
 tar xzf hugo.tgz
-mv hugo_0.15_linux_386/hugo_0.15_linux_386 hugo
+mv hugo_0.19_linux_386/hugo_0.19_linux_386 hugo
 chmod +x hugo
